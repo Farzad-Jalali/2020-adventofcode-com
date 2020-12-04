0aff433d585e1aceeb82401c0b6ed9b6bca29e8e
Farzad Jalali <Farzad@Jalali.co.uk>
2020-12-04 00:38:16 +0000

    day1 finished

---
+++ AdventOfCode.Days/AdventOfCode.Apps/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using AdventOfCode.Apps.day1;
+
+namespace AdventOfCode.Apps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var task = new Day1();
+            task.orchestrator();
+            
+            Console.WriteLine("press enter to finsih");
+            Console.ReadLine();
+            
+        }
+    }
+}
