45a145168be1b04cc363fafce369a136b1beda0d
Farzad Jalali <Farzad@Jalali.co.uk>
2020-12-10 11:04:30 +0000

    day2

--- 2020/AdventOfCode.Apps/Program.cs
+++ 2020/AdventOfCode.Apps/Program.cs
@@ -8,8 +8,14 @@ namespace AdventOfCode.Apps
         static void Main(string[] args)
         {
 
-            var task = new Day1();
-            task.orchestrator();
+            // var task = new Day1();
+            // task.orchestrator();
+            
+            
+            var task = new Day2b();
+            Console.WriteLine($"day2 -a : { task.Answer()}");
+           
+            
             
             Console.WriteLine("press enter to finsih");
             Console.ReadLine();
